33f65d69c95ded5d8c78cd205c64a62619f35a01
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2024-01-20 21:03:30 +0530

    Initial Commit

---
+++ MapKit_iOS17/MapKit_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  MapKit_iOS17App.swift
+//  MapKit_iOS17
+//
+//  Created by Adarsh Shukla on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MapKit_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
